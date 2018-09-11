8098f45bc9b4a16073337eb00a097eac0083d3f5
Leo-admin <Leo-admin@DESKTOP-24LK678>
2018-09-11 10:39:37 +0300

    "add cancell request "

--- src/main/java/com/company/project/RemoteServerConnectApplication.java
+++ src/main/java/com/company/project/RemoteServerConnectApplication.java
@@ -8,27 +8,35 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.company.project.component.DataTransferObject;
 import com.company.project.component.HttpRequestSender;
 import com.company.project.component.RequestType;
+import java.util.Random;
 
 @SpringBootApplication
 public class RemoteServerConnectApplication implements CommandLineRunner {
-		
-	@Autowired
-	HttpRequestSender sender;
-	
 
-	public static void main(String[] args) {
-		SpringApplication.run(RemoteServerConnectApplication.class, args);		
-	}
-	
-	@Override
-	public void run(String... arg0) throws Exception {
-		
-		DataTransferObject dto = new DataTransferObject(21, 555, 45887, 100);
-		
-		sender.sendHttpRequest(dto, RequestType.VERIFY);
-		sender.sendHttpRequest(dto, RequestType.PAYMENT);
-		sender.sendHttpRequest(dto, RequestType.STATUS);
-			
-	}
-	
+    @Autowired
+    HttpRequestSender sender;
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteServerConnectApplication.class, args);
+    }
+
+    @Override
+    public void run(String... arg0) throws Exception {
+        Random random = new Random();
+        int id = 753;
+        
+        
+            
+        DataTransferObject dto = new DataTransferObject(753, 380674060606L, 45887, random.nextInt(500)+1, 240);
+
+        sender.sendHttpRequest(dto, RequestType.VERIFY);
+	sender.sendHttpRequest(dto, RequestType.PAYMENT);
+	sender.sendHttpRequest(dto, RequestType.STATUS);
+//        sender.sendHttpRequest(dto, RequestType.CANCEL);
+  
+        
+
+
+    }
+
 }
